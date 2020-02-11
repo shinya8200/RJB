469336748cbcca03d11782881da1a6db842ba2f6
shinya8200 <shinya8200@hotmail.com>
2020-02-11 18:03:51 +0900

    Create ManifestInput.cs

---
+++ RulerForJBook/ManifestInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RulerJB
+{
+	public partial class ManifestInput : Form
+	{
+		private string _url;
+
+		public ManifestInput()
+		{
+			InitializeComponent();
+		}
+
+		public string FileURL { get { return _url; } }
+
+		private void buttonOpen_Click(object sender, EventArgs e)
+		{
+			_url = textBoxURL.Text;
+			this.Close();
+		}
+	}
+}
