3cac527bd1bce299ef62aacf052d16a603b63ad6
shinya8200 <shinya8200@hotmail.com>
2020-02-11 18:03:45 +0900

    Create IXmlProject.cs

---
+++ RulerForJBook/IXmlProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace RulerJB
+{
+	/// <summary>
+	/// XML関連のインターフェース
+	/// </summary>
+	interface IXmlProject
+	{
+		/// <summary>Xmlでの書き込み</summary>
+		/// <param name="writer">書き込み用ストリーム</param>
+		/// <param name="keyword">キーワード</param>
+		/// <returns>成否</returns>
+		bool ToXmlWriter(XmlWriter writer, string keyword);
+
+	}
+}
