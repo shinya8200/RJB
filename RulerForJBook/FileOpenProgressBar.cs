2c49607f67b44f01dcae087ab8ee9dd5e87ecb49
shinya8200 <shinya8200@hotmail.com>
2020-02-11 18:01:58 +0900

    Create FileOpenProgressBar.cs

---
+++ RulerForJBook/FileOpenProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RulerJB
+{
+	public partial class FileOpenProgressBar : Form
+	{
+		public int value { private get;  set; }
+
+		public FileOpenProgressBar()
+		{
+			InitializeComponent();
+		}
+
+		public void UpdateBar()
+		{
+			progressBarFileOpen.Value = value;
+			progressBarFileOpen.Refresh();
+		}
+	}
+}
